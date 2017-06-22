d105c2ad45a13a0f5df13226f04c119ca10c2e74
Hellmight <bh6402@haileybury.com>
2017-06-22 09:23:04 +0100

    Update JP1337.py

--- JP1337.py
+++ JP1337.py
@@ -7,13 +7,13 @@ import time
 userID = ("JP1337")
 stockID = ("TSLA")
 	
-	compCode = stockID
-	abPrice = LennyStocksAPI.getPrice(compCode)
-	feeVar = LennyStocksAPI.getFee(compCode)
-	print "Company:",compCode,"\t Price per Stock:",abPrice,"\t Transaction Fee:",feeVar
+compCode = stockID
+abPrice = LennyStocksAPI.getPrice(compCode)
+feeVar = LennyStocksAPI.getFee(compCode)
+print "Company:",compCode,"\t Price per Stock:",abPrice,"\t Transaction Fee:",feeVar
 	
-	balance = LennyStocksAPI.getBalance(userID)
-	print "Balance:",balance
+balance = LennyStocksAPI.getBalance(userID)
+print "Balance:",balance
 	
 s_amount = (balance - feeVar)/abPrice
 finalAmount = math.floor(s_amount)
