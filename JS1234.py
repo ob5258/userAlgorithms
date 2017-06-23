7c636581e2f5a9aaa081f2ca8b6a039a878d6a94
jonspav <jon.spavin@spavin.co.uk>
2017-06-23 14:12:06 +0100

    Update JS1234.py

--- JS1234.py
+++ JS1234.py
@@ -9,12 +9,10 @@ UID = "JS1234"
 while 1 > 0:
 	now_time = now.time()
 	if now_time >= time(9,30) and now_time <=(9,35):				
-		
-	
-	
-	LennyStocksAPI.buyStocks(UID, "AAPL", 2)
-	time.sleep(600)
-	LennyStocksAPI.sellStocks(UID, "AAPL", 2)
+		LennyStocksAPI.buyStocks(UID, "AAPL", 2)
+		time.sleep(600)
+	if now_time >= time(15,30) and now_time <=(15,35):
+		LennyStocksAPI.sellStocks(UID, "AAPL", 2)
 	
 	
 
