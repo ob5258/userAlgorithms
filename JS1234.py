e4c8d2c538f81a95c94b669c0c6f378dd7cb2d3c
jonspav <jon.spavin@spavin.co.uk>
2017-06-23 14:09:03 +0100

    Update JS1234.py

--- JS1234.py
+++ JS1234.py
@@ -1,8 +1,16 @@
 import LennyStocksAPI
 import time
+
+from datetime import datetime, time
+now = datetime.now()
+
 UID = "JS1234"
 
 while 1 > 0:
 	LennyStocksAPI.buyStocks(UID, "AAPL", 2)
 	time.sleep(600)
 	LennyStocksAPI.sellStocks(UID, "AAPL", 2)
+	
+	
+
+	
