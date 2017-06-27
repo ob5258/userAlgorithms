9edf53aac49f7399bbf2a3cc3705d944875813a9
ob5258 <ob5258@haileybury.com>
2017-06-27 12:45:27 +0100

    Update AM3169.py

--- AM3169.py
+++ AM3169.py
@@ -2,7 +2,7 @@ import LennyStocksAPI
 
 import time
 UID = "AM3169"
-while 1 > 0: 
+while 1 > 0:
   LennyStocksAPI.buyStocks(UID, "AAPL", 2)
   time.sleep(600)
   LennyStocksAPI.sellStocks(UID, "AAPL", 2)
