5a77e86901171e0a30e08928be0ce84e4b57dbd6
Hellmight <bh6402@haileybury.com>
2017-06-27 12:18:11 +0100

    Create RT1234.py

---
+++ RT1234.py
@@ -0,0 +1,22 @@
+import LennyStocksAPI
+import time
+
+UID = "RT1234"
+    
+LennyStocksAPI.buyStocks(UID, "AMZN", 1)
+LennyStocksAPI.buyStocks(UID, "AAPL", 1)
+LennyStocksAPI.buyStocks(UID, "BA", 1)
+LennyStocksAPI.buyStocks(UID, "PRSM", 1)
+LennyStocksAPI.buyStocks(UID, "CSCO", 1)
+LennyStocksAPI.buyStocks(UID, "ECK", 1)
+LennyStocksAPI.buyStocks(UID, "FB", 1)
+LennyStocksAPI.buyStocks(UID, "GOOGL", 1)
+LennyStocksAPI.buyStocks(UID, "INTC", 1)
+LennyStocksAPI.buyStocks(UID, "ISRG", 1)
+LennyStocksAPI.buyStocks(UID, "MSFT", 1)
+LennyStocksAPI.buyStocks(UID, "SNAP", 1)
+LennyStocksAPI.buyStocks(UID, "TSLA", 1)
+LennyStocksAPI.buyStocks(UID, "TWLO", 1)
+LennyStocksAPI.buyStocks(UID, "TWTR", 1)
+LennyStocksAPI.buyStocks(UID, "VOD", 1)
+LennyStocksAPI.buyStocks(UID, "WB", 1)
