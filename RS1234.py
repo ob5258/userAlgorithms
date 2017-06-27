e3be126655a46b7dda8bb42e769be19cc3edb7ab
Hellmight <bh6402@haileybury.com>
2017-06-27 12:17:55 +0100

    Create RS1234.py

---
+++ RS1234.py
@@ -0,0 +1,11 @@
+import LennyStocksAPI
+import time
+
+UID = "RS1234"
+    
+LennyStocksAPI.buyStocks(UID, "MKS", 1)
+LennyStocksAPI.buyStocks(UID, "MRW", 1)
+LennyStocksAPI.buyStocks(UID, "OCDO", 1)
+LennyStocksAPI.buyStocks(UID, "SBRY", 1)
+LennyStocksAPI.buyStocks(UID, "TSCO", 1)
+
