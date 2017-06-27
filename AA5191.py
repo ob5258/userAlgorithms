66e5c5741281d64c18fbba0aadffaed8b8dfb797
Hellmight <bh6402@haileybury.com>
2017-06-27 12:39:16 +0100

    Update AA5191.py

--- AA5191.py
+++ AA5191.py
@@ -3,7 +3,7 @@ import time
 
 UID = "AA5191"
 
-while 1 > 0: 
-  LennyStocksAPI.buyStocks(UID, "AAPL", 2) 
+while 1 > 0:
+  LennyStocksAPI.buyStocks(UID, "AAPL", 2)
   time.sleep(600)
   LennyStocksAPI.sellStocks(UID, "AAPL", 2)
