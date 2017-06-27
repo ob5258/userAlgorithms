6009b4cf563802b4de3ffff2b7eb75ce1242bc6b
Hellmight <bh6402@haileybury.com>
2017-06-27 12:17:33 +0100

    Create RB1234.py

---
+++ RB1234.py
@@ -0,0 +1,28 @@
+import LennyStocksAPI
+import time
+
+UID = "RB1234"
+
+LennyStocksAPI.buyStocks(UID, "AMZN", 1)
+LennyStocksAPI.buyStocks(UID, "AAPL", 1)
+LennyStocksAPI.buyStocks(UID, "BA", 1)
+LennyStocksAPI.buyStocks(UID, "PRSM", 1)
+LennyStocksAPI.buyStocks(UID, "CSCO", 1)
+LennyStocksAPI.buyStocks(UID, "ECK", 1)
+LennyStocksAPI.buyStocks(UID, "FB", 1)
+LennyStocksAPI.buyStocks(UID, "GOOGL", 1)
+LennyStocksAPI.buyStocks(UID, "INTC", 1)
+LennyStocksAPI.buyStocks(UID, "ISRG", 1)
+LennyStocksAPI.buyStocks(UID, "MSFT", 1)
+LennyStocksAPI.buyStocks(UID, "SNAP", 1)
+LennyStocksAPI.buyStocks(UID, "TSLA", 1)
+LennyStocksAPI.buyStocks(UID, "TWLO", 1)
+LennyStocksAPI.buyStocks(UID, "TWTR", 1)
+LennyStocksAPI.buyStocks(UID, "VOD", 1)
+LennyStocksAPI.buyStocks(UID, "WB", 1)   
+LennyStocksAPI.buyStocks(UID, "MKS", 1)
+LennyStocksAPI.buyStocks(UID, "MRW", 1)
+LennyStocksAPI.buyStocks(UID, "OCDO", 1)
+LennyStocksAPI.buyStocks(UID, "SBRY", 1)
+LennyStocksAPI.buyStocks(UID, "TSCO", 1)
+
